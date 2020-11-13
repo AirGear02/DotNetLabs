ea727ce5d0b0b2a6f38a17cd942b098531db2428
Andriy Drobot <andriydrobot@gmail.com>
2020-11-13 20:53:33 +0200

    Added Lab4 and Lab5

---
+++ Lab5/Lab5/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Lab5.Models;
+
+namespace Lab5.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public PhotoStudioContext Context { get; }
+
+        public IndexModel(ILogger<IndexModel> logger, PhotoStudioContext context)
+        {
+            _logger = logger;
+            Context = context;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ Lab5/Lab5/Pages/Orders/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Lab5.Models;
+
+namespace Lab5.Pages.Orders
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Lab5.Models.PhotoStudioContext _context;
+
+        public CreateModel(Lab5.Models.PhotoStudioContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["ClientId"] = new SelectList(_context.Clients, "Id", "FullName");
+        ViewData["OptionId"] = new SelectList(_context.Options, "Id", "Title");
+            return Page();
+        }
+
+        [BindProperty]
+        public Order Order { get; set; }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Orders.Add(Order);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
